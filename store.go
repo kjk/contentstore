ed322ca6ec1803a0d0bd9fe3223fa59390b4f7af
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-06-22 17:38:30 -0700

    mostly finished

--- store.go
+++ store.go
@@ -61,8 +61,12 @@ type Store struct {
 	idxFile         *os.File
 	idxCsvWriter    *csv.Writer
 	currSegmentFile *os.File
-	currSegment     int
+	currSegmentNo   int
 	currSegmentSize int
+	// we cache file descriptor for one segment file (in addition to current
+	// segment file) to reduce file open/close for Get()
+	cachedSegmentFile *os.File
+	cachedSegmentNo   int
 }
 
 func idxFilePath(baseFilePath string) string {
@@ -108,7 +112,7 @@ func appendIntIfNotExists(aPtr *[]int, x int) {
 	*aPtr = a
 }
 
-func (store *Store) readExistingData() error {
+func (store *Store) readIndex() error {
 	// at this point idx file must exist
 	fidx, err := os.Open(idxFilePath(store.baseFilePath))
 	if err != nil {
@@ -146,31 +150,49 @@ func (store *Store) readExistingData() error {
 		if !u.PathExists(path) {
 			return errSegmentFileMissing
 		}
+		store.currSegmentNo = nSegment
 	}
 	return nil
 }
 
 func NewStoreWithLimit(baseFilePath string, maxSegmentSize int) (store *Store, err error) {
 	store = &Store{
-		baseFilePath:   baseFilePath,
-		blobs:          make([]Blob, 0),
-		maxSegmentSize: maxSegmentSize,
+		baseFilePath:    baseFilePath,
+		blobs:           make([]Blob, 0),
+		maxSegmentSize:  maxSegmentSize,
+		cachedSegmentNo: -1,
 	}
 	idxPath := idxFilePath(baseFilePath)
 	idxExists := u.PathExists(idxPath)
 	if idxExists {
-		// Note: we have enough data in segment files to reconstruct the index
-		// in the rare case that index got deleted but segment files did not.
-		// The logic to do that is not implemented.
-		if err = store.readExistingData(); err != nil {
+		if err = store.readIndex(); err != nil {
 			return nil, err
 		}
 	}
-
 	if store.idxFile, err = os.OpenFile(idxPath, os.O_WRONLY|os.O_APPEND, 0644); err != nil {
 		return nil, err
 	}
-
+	segmentPath := segmentFilePath(store.baseFilePath, store.currSegmentNo)
+	stat, err := os.Stat(segmentPath)
+	if err != nil {
+		// TODO: fail if error is different than "file doesn't exist"
+		if store.currSegmentNo != 0 {
+			store.Close()
+			return nil, errSegmentFileMissing
+		}
+		store.currSegmentFile, err = os.Create(segmentPath)
+		if err != nil {
+			store.Close()
+			return nil, err
+		}
+	} else {
+		store.currSegmentSize = int(stat.Size())
+		store.currSegmentFile, err = os.OpenFile(segmentPath, os.O_APPEND|os.O_RDWR, 0644)
+		if err != nil {
+			store.Close()
+			return nil, err
+		}
+	}
 	return store, nil
 }
 
@@ -190,6 +212,18 @@ func closeFilePtr(filePtr **os.File) (err error) {
 func (store *Store) Close() {
 	closeFilePtr(&store.idxFile)
 	closeFilePtr(&store.currSegmentFile)
+	closeFilePtr(&store.cachedSegmentFile)
+}
+
+func writeBlobRec(csvWriter *csv.Writer, blob *Blob) error {
+	sha1Str := hex.EncodeToString(blob.sha1[:])
+	rec := []string{
+		sha1Str,
+		strconv.Itoa(blob.nSegment),
+		strconv.Itoa(blob.offset),
+		strconv.Itoa(blob.size),
+	}
+	return csvWriter.Write(rec)
 }
 
 func (store *Store) Put(d []byte) (id string, err error) {
@@ -201,23 +235,64 @@ func (store *Store) Put(d []byte) (id string, err error) {
 	if _, ok := store.sha1ToBlobNo[id]; ok {
 		return id, nil
 	}
-	// TODO: save the blob to current segment
-	// TODO: when exceeded segment size, switch to a new segment
-	return "", errors.New("NYI")
+	blob := Blob{
+		size:     len(d),
+		offset:   store.currSegmentSize,
+		nSegment: store.currSegmentNo,
+	}
+	copy(blob.sha1[:], idBytes)
+
+	if _, err = store.currSegmentFile.Write(d); err != nil {
+		return "", err
+	}
+	if err = store.currSegmentFile.Sync(); err != nil {
+		return "", err
+	}
+	store.currSegmentSize += blob.size
+	if store.currSegmentSize >= store.maxSegmentSize {
+		// filled current segment => create a new one
+		err = store.currSegmentFile.Close()
+		if err != nil {
+			return "", err
+		}
+		store.currSegmentNo += 1
+		store.currSegmentSize = 0
+		path := segmentFilePath(store.baseFilePath, store.currSegmentNo)
+		store.currSegmentFile, err = os.Create(path)
+		if err != nil {
+			return "", err
+		}
+	}
+	if err = writeBlobRec(store.idxCsvWriter, &blob); err != nil {
+		return "", err
+	}
+	store.blobs = append(store.blobs, blob)
+	return id, nil
 }
 
-// TODO: cache file descriptor
-func readFromFile(path string, offset, size int) ([]byte, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
+func readFromFile(file *os.File, offset, size int) ([]byte, error) {
 	res := make([]byte, size, size)
-	if _, err := f.ReadAt(res, int64(offset)); err != nil {
+	if _, err := file.ReadAt(res, int64(offset)); err != nil {
 		return nil, err
 	}
 	return res, nil
+}
+
+func (store *Store) getSegmentFile(nSegment int) (*os.File, error) {
+	if nSegment == store.currSegmentNo {
+		return store.currSegmentFile, nil
+	}
+	if nSegment == store.cachedSegmentNo {
+		return store.cachedSegmentFile, nil
+	}
+	closeFilePtr(&store.cachedSegmentFile)
+	path := segmentFilePath(store.baseFilePath, nSegment)
+	var err error
+	if store.cachedSegmentFile, err = os.Open(path); err != nil {
+		return nil, err
+	}
+	store.cachedSegmentNo = nSegment
+	return store.cachedSegmentFile, nil
 }
 
 func (store *Store) Get(id string) ([]byte, error) {
@@ -229,6 +304,9 @@ func (store *Store) Get(id string) ([]byte, error) {
 		return nil, errNotFound
 	}
 	blob := store.blobs[blobNo]
-	path := segmentFilePath(store.baseFilePath, blob.nSegment)
-	return readFromFile(path, blob.offset, blob.size)
+	segmentFile, err := store.getSegmentFile(blob.nSegment)
+	if err != nil {
+		return nil, err
+	}
+	return readFromFile(segmentFile, blob.offset, blob.size)
 }
